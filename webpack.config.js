87d05a246a24de88169cd493b75d4b299df5b239
wojtek2kdev <wojtek2kdev@gmail.com>
2018-05-29 19:53:08 +0200

    Frontend building configuration has been ended.

--- webpack.config.js
+++ webpack.config.js
@@ -43,14 +43,14 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': ExtractTextPlugin.extract({
+            'scss': process.env.NODE_ENV === 'production' ? ExtractTextPlugin.extract({
               use: 'css-loader!sass-loader',
               fallback: 'vue-style-loader'
-            }),
-            'sass': ExtractTextPlugin.extract({
+            }) : ['vue-style-loader','css-loader','sass-loader'],
+            'sass': process.env.NODE_ENV === 'production' ? ExtractTextPlugin.extract({
               use: 'css-loader!sass-loader?indentedSyntax',
               fallback: 'vue-style-loader'
-            })
+            }) : ['vue-style-loader','css-loader','sass-loader']
           }
           // other vue-loader options go here
         }
