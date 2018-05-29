5f25b48691c09661126baff724e771a03bd10513
wojtek2kdev <wojtek2kdev@gmail.com>
2018-05-29 20:18:47 +0200

    ElementUI prepared and ready to work

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+// my components
 import App from './App.vue'
+
+Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
