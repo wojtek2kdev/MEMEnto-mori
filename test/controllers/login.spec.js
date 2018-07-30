37072dd4e99e0f055b9694ac89ce96f519e976a4
wojtek2kdev <wojtek2kdev@gmail.com>
2018-07-30 10:48:54 +0200

    Added login test if auth isn't correct

--- test/controllers/login.spec.js
+++ test/controllers/login.spec.js
@@ -2,38 +2,55 @@ const LoginController = require('../../src/controllers/LoginController');
 const User = require('../../src/models/user');
 const expect = require('chai').expect;
 
-const auth = LoginController.authentication;
+const createUser = async (username, password) => {
+    
+    User.truncate();
+    
+    await User.create({
+        username: username,
+        password: password,
+        password_confirmation: password
+    });
+
+}
 
 describe(`Authentication login form`, () => {
     
-    before(async () => {
-
-        User.truncate();
-    
-        await User.create({
-            username: `test_user`,
-            password: `P4ssword!`,
-            password_confirmation: `P4ssword!`
-        });
-        
-    });
+    before(() => createUser(`test_user`, `P4ssword!`));
 
     it(`Authentication should be correct`, () => {
 
         const user = {
             username: `test_user`,
             password: `P4ssword!`,
-            password_confirmation:`P4ssword!`
         }
 
-        const isUserExists = auth(user).isUserExists();
+        const login = new LoginController(user);
 
-        expect(isUserExists).to.be.true;
+        const authResult = login.auth();
+
+        expect(authResult).to.be.true;
 
     });
 
+    before(() => createUser(`test_user`, `P4ssword!`));
+
     it(`If authentication isn't correct`, () => {
         
+        const user = {
+            username: `test_user`,
+            password: `P4ssword`,
+        };
+
+        const login = new LoginController(user);
+
+        const authResult = login.auth();
+
+        const error = new Error(`Username or password incorrect`);
+        error.status = 401;
+
+        expect(authResult).to.be.equal(error);
+
     });
 
 });
